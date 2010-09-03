54b149974943b1b8926a96d20d2252c13a225e8f
afa <afa.alone@gmail.com>
2010-09-03 17:15:30 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codedeco::Application.config.secret_token = '5af55199bc0fede19bd411001f22294828454e503ad897d4b177f15083d4e87e3ce960109ea3b65f3cc37c97ab85211108e85e638e5f0470125f76f3085929fa'
